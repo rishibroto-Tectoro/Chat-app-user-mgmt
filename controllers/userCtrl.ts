4e0f86d3fae6509c76a3d33a8edcfef453f60861
salman-kutty <salmansalimkutty@gmail.com>
2023-02-28 11:51:55 +0530

    changes according to feedback

--- controllers/userCtrl.ts
+++ controllers/userCtrl.ts
@@ -6,69 +6,26 @@ const prisma = new PrismaClient();
 
 export const getAllUser = async (req: Request, res: Response) => {
     try {
-        const obj= filtering(req.body);
-        console.log("Object --> ",obj)
-        const data = await prisma.user.findMany({skip:(req.body.skip -1) * req.body.take ,take:req.body.take , where: obj,include:{groupOwned:true} });
+        const data = await prisma.user.findMany({
+            skip: (req.body.skip - 1) * req.body.take, take: req.body.take,
+            where: req.body.where
+            , include: { groupOwned: true }
+        });
         res.status(200).json({ status: "Success", response: data, message: null });
 
-    } catch (err:any) {
+    } catch (err: any) {
         res.status(400).json({ status: "Failed", message: err.message, response: null });
     }
-}
-
-const filtering=(body:any)=>{
-    let whereObj:any={};
-    if(body.id){
-        whereObj["id"]=body.id
-    }
-    if(body.ref){
-        whereObj["ref"]={contains:body.ref}
-    }
-    if(body.name){
-        whereObj["name"]={contains:body.name}
-    }
-    if(body.email){
-        whereObj["email"]={contains:body.email}
-    }
-    if(body.phoneNum){
-        whereObj["phoneNum"]={contains:body.phoneNum}
-    }
-    if(body.createdAt){
-        whereObj['createdAt']={contains:body.createdAt}
-    }
-    if(body.hasOwnProperty('is_active')){
-        whereObj["is_active"]=body.is_active
-    }
-    if(body.updatedAt){
-        whereObj['updatedAt']={contains:body.updatedAt}
-    }
-    if(body.lastLogin){
-        whereObj['lastLogin']={contains:body.lastLogin}
-    }
-    return whereObj;
 }
 
 export const updateGroup = async (req: Request, res: Response) => {
     try {
-        
-        const obj=updateData(req.body);
-        console.log(obj)
-        const data = await prisma.group.update({where:{id:req.body.id},data:obj})
+        const data = await prisma.group.update({ where: { id: req.body.id }, data: req.body })
         res.status(200).json({ status: "Success", response: data, message: null });
-    } catch (err:any) {
+    } catch (err: any) {
         res.status(400).json({ status: "Failed", message: err.message, response: null });
     }
 }
 
-const updateData=(body:any)=>{
-    console.log(body)
-    let whereObj:any={}
-    if(body.name){
-        whereObj["name"]=body.name
-    }
-    if(body.hasOwnProperty('is_active')){
-        whereObj["is_active"]=body.is_active
-    }
-    return whereObj;
-}
+
 
